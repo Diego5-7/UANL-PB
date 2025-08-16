20986d01ea0542cc7ae847320b92a25829dc39cd
Diego5-7 <diego.lopezmrn@uanl.edu.mx>
2025-08-15 21:45:39 -0600

    Problema 3: Operaciones con variables
    Define las variables x = 3 y y = 5. Luego, realiza las siguientes operaciones en orden:
    • Actualiza el valor de x como x = x + y.
    • Actualiza el valor de y como y = 2 * x - y.
    • Calcula el valor de x² + y².

---
+++ Introduccion 1-10/3.- Operaciones con variables.py
@@ -0,0 +1,13 @@
+x = 3
+y = 5
+print("Valores iniciales x:", x, ", y:", y)
+
+x = x + y
+print("El nuevo valor de x es:", x)
+
+y = 2 * x - y
+print("El nuevo valor de y es:", y)
+
+resultado = x**2 + y**2
+print("El resultado de la operacion es:", resultado)
+
