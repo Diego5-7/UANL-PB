f8503ba4611cd8a5465f710ac4fc48f98d492da6
Diego5-7 <diego.lopezmrn@uanl.edu.mx>
2025-08-15 21:58:48 -0600

    Problema 5. Comparación de cadenas
    Escribe un programa que contenga las variables a = 'Pedro' y b = 'pedro'.
    Ejecuta y analiza el resultado de las siguientes instrucciones:
    print( a == b)

---
+++ Introduccion 1-10/5.- Comparación de cadenas.py
@@ -0,0 +1,7 @@
+
+a = 'Pedro'
+b = 'pedro'
+print("a == b:", a == b)  
+
+Conclusion = " Las cadenas son diferentes por que Python distingue entre las mayusculas y minúsculas."
+print("Conclusion:", Conclusion)
