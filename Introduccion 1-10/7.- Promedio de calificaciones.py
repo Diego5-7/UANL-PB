3b2ae7af4e324c1e70dc16457eeb35b9686ed6a3
Diego5-7 <diego.lopezmrn@uanl.edu.mx>
2025-08-15 22:10:46 -0600

    Problema 7: Promedio de calificaciones
    Almacena las siguientes calificaciones en variables: 7, 6.5, 8.4, 9, 4.
    Calcula el promedio y muestra el resultado en pantalla.

---
+++ Introduccion 1-10/7.- Promedio de calificaciones.py
@@ -0,0 +1,8 @@
+Calificacion_1 = 7
+Calificacion_2 = 6.5
+Calificacion_3 = 8.4
+Calificacion_4 = 9
+Calificacion_5 = 4
+promedio = (Calificacion_1 + Calificacion_2 + Calificacion_3 + Calificacion_4 + Calificacion_5) / 5
+print("Promedio de calificaciones:", promedio)
+
