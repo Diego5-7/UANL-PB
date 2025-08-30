ad9b16290f7ff63e98c6cc3f2db84b9028d28df4
Diego5-7 <diego.lopezmrn@uanl.edu.mx>
2025-08-29 18:33:45 -0600

    17.- Cálculo de beneficio.py

--- Datos, variables, print e input 11-25/17.- Cálculo de beneficio.py
+++ Datos, variables, print e input 11-25/17.- Cálculo de beneficio.py
@@ -1,11 +1,12 @@
-precio_venta = float(input("Ingrese el precio de venta por pieza: "))
-cantidad_vendida = int(input("Ingrese la cantidad vendida: "))
-costo_fijo = float(input("Ingrese el costo fijo total: "))
-costo_variable = float(input("Ingrese el costo variable por pieza: "))
+precio_venta = float(input("Ingresa el precio de venta por pieza: "))
+cantidad = int(input("Ingresa la cantidad vendida: "))
+costo_fijo = float(input("Ingresa el costo fijo: "))
+costo_variable = float(input("Ingresa el costo variable por pieza: "))
 
-ingreso_total = precio_venta * cantidad_vendida
-costo_total = costo_fijo + (costo_variable * cantidad_vendida)
+ingreso_total = precio_venta * cantidad
+
+costo_total = costo_fijo + (costo_variable * cantidad)
 
 beneficio = ingreso_total - costo_total
 
-print(f"\nEl beneficio total es: ${beneficio:.2f}")
+print("El beneficio total es: $", beneficio)
